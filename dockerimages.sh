68f41ba88e9c67ca558ff1fcdb71d28095d54b18
Tate Exon <tate.exon@kount.com>
2020-11-20 15:06:53 -0700

    update to jmeter 5.3 and add user to dockerfiles

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
-docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
+docker build --tag="tateexon/jmeter-base:latest" -f Dockerfile-base .
+docker build --tag="tateexon/jmeter-master:latest" -f Dockerfile-master .
+docker build --tag="tateexon/jmeter-slave:latest" -f Dockerfile-slave .
+docker build --tag="tateexon/jmeter-reporter:latest" -f Dockerfile-reporter .
---
+++ dockerimagespush.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+docker push tateexon/jmeter-base:latest
+docker push tateexon/jmeter-master:latest
+docker push tateexon/jmeter-slave:latest
+docker push tateexon/jmeter-reporter:latest
