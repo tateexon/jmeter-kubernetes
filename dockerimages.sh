f7cdb39cbfd0f1343fae8da061885f094c2f5a4b
Tate Exon <tate.exon@kount.com>
2020-11-20 15:35:22 -0700

    add influx image with a user

--- dockerimages.sh
+++ dockerimages.sh
@@ -4,3 +4,4 @@ docker build --tag="tateexon/jmeter-base:latest" -f Dockerfile-base .
 docker build --tag="tateexon/jmeter-master:latest" -f Dockerfile-master .
 docker build --tag="tateexon/jmeter-slave:latest" -f Dockerfile-slave .
 docker build --tag="tateexon/jmeter-reporter:latest" -f Dockerfile-reporter .
+docker build --tag="tateexon/jmeter-influx:latest" -f Dockerfile-influx .
--- dockerimagespush.sh
+++ dockerimagespush.sh
@@ -4,3 +4,4 @@ docker push tateexon/jmeter-base:latest
 docker push tateexon/jmeter-master:latest
 docker push tateexon/jmeter-slave:latest
 docker push tateexon/jmeter-reporter:latest
+docker push tateexon/jmeter-influx:latest
